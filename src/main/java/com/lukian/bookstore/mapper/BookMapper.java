097fe11fca0b0826eb662fa37c379f5b84dcce07
Lukian Muzyka <lukianmuzyka@gmail.com>
2024-02-11 10:34:09 +0200

    added separate config for mapper

--- src/main/java/com/lukian/bookstore/mapper/BookMapper.java
+++ src/main/java/com/lukian/bookstore/mapper/BookMapper.java
@@ -1,14 +1,18 @@
 package com.lukian.bookstore.mapper;
 
+import com.lukian.bookstore.config.MapperConfig;
 import com.lukian.bookstore.dto.BookDto;
 import com.lukian.bookstore.dto.CreateBookRequestDto;
 import com.lukian.bookstore.model.Book;
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
-@Mapper(componentModel = "spring")
+@Mapper(config = MapperConfig.class)
+@Component
 public interface BookMapper {
 
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
 }
+
