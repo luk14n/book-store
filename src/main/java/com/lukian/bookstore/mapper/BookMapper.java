f02ab94cfd5519f82f3841546318615522e1c090
Lukian Muzyka <lukianmuzyka@gmail.com>
2024-02-11 09:21:53 +0200

    adjusted mapper

--- src/main/java/com/lukian/bookstore/mapper/BookMapper.java
+++ src/main/java/com/lukian/bookstore/mapper/BookMapper.java
@@ -4,12 +4,10 @@ import com.lukian.bookstore.dto.BookDto;
 import com.lukian.bookstore.dto.CreateBookRequestDto;
 import com.lukian.bookstore.model.Book;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
 
-    @Mapping(target = "id", ignore = true)
     BookDto toDto(Book book);
 
     Book toModel(CreateBookRequestDto requestDto);
