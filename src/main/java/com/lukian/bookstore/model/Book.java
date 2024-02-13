cacc25482efab5a287dcdb14d98fd803dfbc14a6
Lukian Muzyka <lukianmuzyka@gmail.com>
2024-02-13 10:49:31 +0200

    implemented soft-delete

--- src/main/java/com/lukian/bookstore/model/Book.java
+++ src/main/java/com/lukian/bookstore/model/Book.java
@@ -1,22 +1,34 @@
 package com.lukian.bookstore.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
+@Table(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String author;
+    @Column(unique = true)
     private String isbn;
     private BigDecimal price;
     private String description;
+    @Column(name = "cover_image")
     private String coverImage;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
 }
+
