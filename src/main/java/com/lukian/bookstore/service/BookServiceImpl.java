7e6d63e72a0e2b54dfc91e073aac71db064b95b6
Lukian Muzyka <lukianmuzyka@gmail.com>
2024-02-14 11:45:28 +0200

    added updateFromDto method to Mapper

--- src/main/java/com/lukian/bookstore/service/BookServiceImpl.java
+++ src/main/java/com/lukian/bookstore/service/BookServiceImpl.java
@@ -41,11 +41,7 @@ public class BookServiceImpl implements BookService {
         Book bookFromDb = bookRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Cannot find book by id: " + id));
 
-        bookFromDb.setAuthor(requestDto.getAuthor());
-        bookFromDb.setTitle(requestDto.getTitle());
-        bookFromDb.setPrice(requestDto.getPrice());
-        bookFromDb.setIsbn(requestDto.getIsbn());
-        bookFromDb.setDescription(requestDto.getDescription());
+        bookMapper.updateFromDto(requestDto, bookFromDb);
 
         return bookMapper.toDto(bookRepository.save(bookFromDb));
     }
