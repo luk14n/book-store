b1da2533f6871bf55305ee7fe2d64b6ab1bf5a3c
Lukian Muzyka <lukianmuzyka@gmail.com>
2024-02-16 12:13:15 +0200

    created CustomGlobalExceptionHandler and added validation to create-dto

--- src/main/java/com/lukian/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/lukian/bookstore/dto/CreateBookRequestDto.java
@@ -1,14 +1,30 @@
 package com.lukian.bookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    public static final String ISBN_REGEXP
+            = "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$";
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @NotBlank
+    @Pattern(regexp = ISBN_REGEXP,
+            message = "Invalid ISBN format")
     private String isbn;
+    @NotNull
+    @Positive
+    @Min(1)
     private BigDecimal price;
+    @NotBlank
     private String description;
     private String coverImage;
 }
