d1ad12e9b6567d3fba532de2d03b0deb2b52028b
Lukian Muzyka <lukianmuzyka@gmail.com>
2024-02-28 12:41:58 +0200

    added soft-delete

--- src/main/java/com/lukian/bookstore/model/ShoppingCart.java
+++ src/main/java/com/lukian/bookstore/model/ShoppingCart.java
@@ -1,5 +1,6 @@
 package com.lukian.bookstore.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -9,9 +10,13 @@ import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.Set;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE shopping_carts SET is_deleted = true WHERE id = ?")
+@Where(clause = "is_deleted=false")
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
@@ -32,4 +37,6 @@ public class ShoppingCart {
      */
     @OneToMany(mappedBy = "shoppingCart")
     private Set<CartItem> cartItems;
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
 }
